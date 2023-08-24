243962ede7be9fe2d7bcaf91022396fcfe1780ff
SevvyYilmaz <yilmaz_S@outlook.de>
2023-08-23 20:52:48 -0700

    validation update

--- AcornPrivateFunds/server/models/product.model.js
+++ AcornPrivateFunds/server/models/product.model.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    price: { type: Number },
-    description: { type: String },
+    title: { 
+        type: String, 
+        required: [true, "Title is required"], 
+        minlength: [2, "Title must be at least 2 characters long!"] },
+    
+    
+        price: { 
+            type: Number,
+            required: [true, "Price is required"],
+        },
+
+    description: { 
+        type: String,
+        required: [true, "Description is required"], 
+        minlength: [2, "Description must be at least 2 characters long!"] 
+    },
 }, { timestamps: true });
 module.exports = mongoose.model('Product', ProductSchema);
 
